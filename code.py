a4840cbc7e44834b449f0a6b515c1864c7534449
MyNameIsChunhuiXu <cx9p9@mail.missouri.edu>
2019-07-29 13:55:06 -0500

    Create code.py

---
+++ code.py
@@ -0,0 +1,11 @@
+Y_train = train["label"]
+
+# Drop 'label' column
+X_train = train.drop(labels = ["label"],axis = 1) 
+
+# free some space
+del train 
+
+g = sns.countplot(Y_train)
+
+Y_train.value_counts()
