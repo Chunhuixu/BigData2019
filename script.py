de8caf9b3bc3a4fd4404a83d3fff5e87e5136874
MyNameIsChunhuiXu <cx9p9@mail.missouri.edu>
2019-08-01 14:09:04 -0500

    Update script.py

--- script.py
+++ script.py
@@ -35,7 +35,8 @@ for i in range(6000):
         training_set[i] = preprocessing.image.img_to_array(img)
 
         
-        model = models.Sequential()
+```
+model = models.Sequential()
 model.add(layers.Conv2D(32, (3, 3), activation='relu',
                        input_shape=(150, 150, 3)))
 model.add(layers.MaxPooling2D((2, 2)))
@@ -48,6 +49,17 @@ model.add(layers.MaxPooling2D((2, 2)))
 model.add(layers.Flatten())
 model.add(layers.Dense(512, activation='relu'))
 model.add(layers.Dense(1, activation='sigmoid'))
+#keras.optimizers.Adam(lr=0.001, beta_1=0.9, beta_2=0.999, epsilon=1e-8) 
 model.compile(loss='binary_crossentropy',
-             optimizer=optimizers.RMSprop(lr=1e-4),
+             optimizer=optimizers.Adam(lr=1e-4),
              metrics=['acc'])
+model.summary()
+```
+```
+history = model.fit(
+    training_set,
+    train_labels,
+    epochs= 30,
+    validation_data=(validation_set,validation_labels),
+)
+```
